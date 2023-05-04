6dc4f1ce897dc9c198a3b964dda1c6cb07fe9312
Mahlon Page <mahlon_page@brown.edu>
2023-05-04 00:18:22 -0400

    cleaned OCR.py and main.py code

--- code/main.py
+++ code/main.py
@@ -1,63 +1,67 @@
-from __future__ import absolute_import
-from matplotlib import pyplot as plt
-
-import os
-import tensorflow as tf
 import numpy as np
-import random
-import math
-from preprocess import get_data
+import tensorflow as tf
 from sklearn import preprocessing
+from preprocess import get_data
 
 
-class Model(tf.keras.Model):
-    def __init__(self, input_shape, num_classes):
-        super(Model, self).__init__()
+def retrieve_data(test_split, data_path):
+    """
+    Function to handle reshaping and formatting of data.
 
-        #Initialize hyperparameters
-        self.num_classes = num_classes
+    Inputs:
+    test_split - Percent of data set asside for testing
+    data_path - Path to data to be used
 
-        #Initialize the model
-        self.model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(32, (3,3), padding="same", input_shape=input_shape),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    Outputs:
+    X_train, X_test, X_val, Y_train, Y_test, Y_val - Divided data (X) and labels (Y)
+    char_encoder - Encoder used to ohe our data
+    encoder_size - Number of ohe values used.
+    """
+    # Import and reshape
+    X_train, X_test, X_val, Y_train, Y_test, Y_val = get_data(test_split, data_path)
 
-            tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="leaky_relu"),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    # Generate a one hot encoder for the dataset
+    char_encoder = preprocessing.LabelEncoder().fit(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1))))
+    encoder_size = np.max(char_encoder.transform(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1)))))
+    orig_shapes  = [Y_train.shape[0], Y_test.shape[0], Y_val.shape[0]]
 
-            tf.keras.layers.Conv2D(128, (3,3), padding="same", activation="leaky_relu"),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+    # Encode from characters to numbers and reshape all of the labels
+    Y_train = char_encoder.transform(Y_train.reshape(-1)).reshape((orig_shapes[0], 4))
+    Y_test  = char_encoder.transform(Y_test.reshape(-1)).reshape((orig_shapes[1], 4))
+    Y_val   = char_encoder.transform(Y_val.reshape(-1)).reshape((orig_shapes[2], 4))
 
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(256, activation='leaky_relu'),
-            tf.keras.layers.Dense(self.num_classes, activation="softmax")
-        ])
+    # Convert input to properly shaped and typed tensors
+    X_train = tf.convert_to_tensor(np.asarray(np.reshape(X_train, (-1, *X_train.shape[-2:]))).astype(np.float32))
+    X_val   = tf.convert_to_tensor(np.asarray(np.reshape(X_val  , (-1, *X_val.shape[-2:]))).astype(np.float32))
+    Y_train = tf.convert_to_tensor(np.asarray(np.reshape(Y_train, (-1))))
+    Y_val   = tf.convert_to_tensor(np.asarray(np.reshape(Y_val  , (-1))))
+
+    # Don't reshape test, because we need to preserve CAPTCHAs but convert to tensor
+    X_test  = tf.convert_to_tensor(np.asarray(X_test).astype(np.float32))
+    Y_test  = tf.convert_to_tensor(np.asarray(Y_test))
+
+    # Expand Dims
+    X_train = tf.expand_dims(X_train, axis=-1)
+    X_val   = tf.expand_dims(X_val  , axis=-1)
+
+    # One hot encode
+    Y_val   = tf.one_hot(Y_val  , depth=encoder_size, axis=-1)
+    Y_train = tf.one_hot(Y_train, depth=encoder_size, axis=-1)
+
+    return X_train, X_test, X_val, Y_train, Y_test, Y_val, char_encoder, encoder_size
 
 
-    def call(self, inputs):
-        return self.model(inputs)
+def create_model(input_shape, encoder_size):
+    """
+    Helper function to instantiate a version of our model with correct inputs
 
+    Inputs:
+    input_shape - shape of the input CAPTCHAs
+    encoder_size - Number of unique characters that can be predicted
 
-def run_tests(model, X_test, Y_test):
-    X_test = np.reshape(X_test, (-1, *X_test.shape[-2:]))
-    Y_test = np.reshape(Y_test, (-1))
-
-    probs = model.predict(X_test, verbose=0)
-    diff = np.argmax(probs, axis=1) - Y_test
-    accuracy = np.where(diff == 0, 1, 0)
-
-    group_acc = np.array_split(diff, diff.shape[0]//4)
-    group_acc = np.apply_along_axis((lambda x: 1 if np.all(x == 0) else 0), axis=1, arr=group_acc)
-
-    return np.mean(accuracy), np.mean(group_acc)
-
-def create_model(input_shape, ohe_size):
+    Output:
+    model - returns our model to be used
+    """
     model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(32, (3,3), padding="same", input_shape=input_shape),
             tf.keras.layers.BatchNormalization(),
@@ -76,48 +80,78 @@ def create_model(input_shape, ohe_size):
 
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(256, activation='leaky_relu'),
-            tf.keras.layers.Dense(ohe_size, activation="softmax")
+            tf.keras.layers.Dense(encoder_size, activation="softmax")
         ])
     return model
 
+
+def get_accuracy(model, X_test, Y_test):
+    """
+    Calculates model accuracy
+
+    Inputs:
+    model - Model you are testing accuracy on
+    X_test - CAPTCHAs to test accuracy on
+    Y_test - True text for each input CAPTCHA
+
+    Output:
+    char_acc - Percentage of the time the model correctly predicts a character
+    captcha_acc - Percentage of the time the model correctly predicts an entire CAPTCHA
+    """
+    # Flattens the data
+    X_test = np.reshape(X_test, (-1, *X_test.shape[-2:]))
+    Y_test = np.reshape(Y_test, (-1))
+
+    # Compares every character prediction to true label and leaves accuracy an array of 1s and 0s
+    # representing whether or not it correctly predicted that character.
+    probs       = model.predict(X_test, verbose=0)
+    diff        = np.argmax(probs, axis=1) - Y_test
+    accuracy    = np.where(diff == 0, 1, 0)
+
+    # Compute character and captcha accuracy from above results
+    char_acc    = np.mean(accuracy)
+    captcha_acc = np.array_split(diff, diff.shape[0]//4)
+    captcha_acc = np.apply_along_axis((lambda x: 1 if np.all(x == 0) else 0), axis=1, arr=captcha_acc)
+    captcha_acc = np.mean(captcha_acc)
+
+    return char_acc, captcha_acc
+
+
+def print_results(model, char_encoder, X_test, Y_test):
+    """
+    Prints an example CAPTCHA prediction vs. actual and prints accuracy
+
+    Inputs:
+    model - Model to get accuracy and example with
+    char_encoder - one hot encoder used to labels of dataset
+    X_test - CAPTCHA images to test with
+    Y_test - True CAPTCHA labels to compare to
+    """
+    output = model.predict(X_test[6], verbose=0)
+    prediction_captcha = char_encoder.inverse_transform(np.argmax(output, axis=1))
+    real_captcha = char_encoder.inverse_transform(Y_test[6])
+
+    # Convert accuracies to percentages
+    char_acc, captcha_acc = get_accuracy(model, X_test, Y_test)
+    char_acc = f"{round(100*char_acc, 2)}%"
+    captcha_acc = f"{round(100*captcha_acc, 2)}%"
+
+    print(f"Example CAPTCHA:     {real_captcha}")
+    print(f"Example Model Guess: {prediction_captcha}")
+    print(f"Character Accuracy:  {char_acc}")
+    print(f"CAPTCHA Accuracy:    {captcha_acc}")
+
+
 def main():
-    #Import and reshape
-    X_train, X_test, X_val, Y_train, Y_test, Y_val = get_data(.3, "./../processed_data/")
-
-    char_encoder = preprocessing.LabelEncoder().fit(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1))))
-    ohe_size = np.max(char_encoder.transform(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1)))))
-    orig_shapes = [Y_train.shape[0], Y_test.shape[0], Y_val.shape[0]]
-    Y_train = char_encoder.transform(Y_train.reshape(-1)).reshape((orig_shapes[0], 4))
-    Y_test = char_encoder.transform(Y_test.reshape(-1)).reshape((orig_shapes[1], 4))
-    Y_val = char_encoder.transform(Y_val.reshape(-1)).reshape((orig_shapes[2], 4))
-
-    #Convert input to properly shaped and typed tensors
-    X_train = tf.convert_to_tensor(np.asarray(np.reshape(X_train, (-1, *X_train.shape[-2:]))).astype(np.float32))
-    X_val   = tf.convert_to_tensor(np.asarray(np.reshape(X_val  , (-1, *X_val.shape[-2:]))).astype(np.float32))
-    Y_train = tf.convert_to_tensor(np.asarray(np.reshape(Y_train, (-1))))
-    Y_val   = tf.convert_to_tensor(np.asarray(np.reshape(Y_val  , (-1))))
-
-    #Don't reshape test, because we need to preserve CAPTCHAs but convert to tensor
-    X_test  = tf.convert_to_tensor(np.asarray(X_test).astype(np.float32))
-    Y_test  = tf.convert_to_tensor(np.asarray(Y_test))
-
-    #Expand Dims
-    X_train = tf.expand_dims(X_train, axis=-1)
-    X_val   = tf.expand_dims(X_val  , axis=-1)
-
-    #One hot encode
-    Y_val   = tf.one_hot(Y_val  , depth=31, axis=-1)
-    Y_train = tf.one_hot(Y_train, depth=31, axis=-1)
+    X_train, X_test, X_val, Y_train, Y_test, Y_val, char_encoder, encoder_size = \
+        retrieve_data(.3, "./processed_data/")
 
     input_shape=(X_train.shape[-3], X_train.shape[-2], X_train.shape[-1])
 
-    # Compile and fit model
-    model = create_model(input_shape, ohe_size)
+    # Fit model
+    model = create_model(input_shape, encoder_size)
     model.compile(loss='categorical_crossentropy',
-                  optimizer=tf.keras.optimizers.Adam(.0001),
-                  metrics= [
-                    tf.keras.metrics.CategoricalCrossentropy()
-                    ])
+                  optimizer=tf.keras.optimizers.Adam(.0001))
     model.fit(
         X_train,
         Y_train,
@@ -129,19 +163,7 @@ def main():
     # Save model for future testing
     model.save('./../models/segmented_2', save_format="h5")
 
-    model = tf.keras.models.load_model("./../models/segmented_2")
-    print(model.summary())
-
-    # Run random CAPTCHA test
-    output = model.predict(X_test[6], verbose=0)
-    print("Secret CAPTCHA:")
-    print(char_encoder.inverse_transform(Y_test[6]))
-    print("Model guess:")
-    print(char_encoder.inverse_transform(np.argmax(output, axis=1)))
-
-    # Print individual and captcha-based accuracy
-    indiv_acc, group_acc = run_tests(model, X_test, Y_test)
-    print(f"Individual accuracy: {round(100*indiv_acc, 2)}% \nCaptcha accuracy: {round(100*group_acc, 2)}%")
+    print_results(model, char_encoder, X_test, Y_test)
 
 
 if __name__ == '__main__':
--- code/ocr.py
+++ code/ocr.py
@@ -1,24 +1,66 @@
-from __future__ import absolute_import
-from matplotlib import pyplot as plt
-
-import os
-import tensorflow as tf
 import numpy as np
-import random
-import math
-from preprocess import get_data_ocr
+import tensorflow as tf
 from sklearn import preprocessing
+from preprocess import get_data_ocr
 
 
-class Model(tf.keras.Model):
-    def __init__(self, input_shape, num_classes):
-        super(Model, self).__init__()
+def retrieve_data(test_split, data_path):
+    """
+    Function to handle reshaping and formatting of data.
 
-        #Initialize hyperparameters
-        self.num_classes = num_classes
+    Inputs:
+    test_split - Percent of data set asside for testing
+    data_path - Path to data to be used
 
-        #Initialize the model
-        self.model = tf.keras.models.Sequential([
+    Outputs:
+    X_train, X_test, X_val, Y_train, Y_test, Y_val - Divided data (X) and labels (Y)
+    char_encoder - Encoder used to ohe our data
+    encoder_size - Number of ohe values used.
+    """
+    # Import and reshape
+    X_train, X_test, X_val, Y_train, Y_test, Y_val = get_data_ocr(test_split, data_path)
+
+    # Generate a one hot encoder for the dataset
+    char_encoder = preprocessing.LabelEncoder().fit(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1))))
+    encoder_size = np.max(char_encoder.transform(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1)))))
+    orig_shapes  = [Y_train.shape[0], Y_test.shape[0], Y_val.shape[0]]
+
+    # Encode from characters to numbers and reshape all of the labels
+    Y_train = char_encoder.transform(Y_train.reshape(-1)).reshape((orig_shapes[0], 4))
+    Y_test  = char_encoder.transform(Y_test.reshape(-1)).reshape((orig_shapes[1], 4))
+    Y_val   = char_encoder.transform(Y_val.reshape(-1)).reshape((orig_shapes[2], 4))
+
+    # Convert input to properly shaped and typed tensors
+    X_train = tf.convert_to_tensor(np.asarray(X_train).astype(np.float32))
+    Y_train = tf.convert_to_tensor(np.asarray(Y_train))
+    X_val   = tf.convert_to_tensor(np.asarray(X_val).astype(np.float32))
+    Y_val   = tf.convert_to_tensor(np.asarray(Y_val))
+
+    # Don't reshape test, because we need to keep letters together as one CAPTCHA, but convert to tensor
+    X_test  = tf.convert_to_tensor(np.asarray(X_test).astype(np.float32))
+    Y_test  = tf.convert_to_tensor(np.asarray(Y_test))
+
+    # Expand Dims
+    X_train = tf.expand_dims(X_train, axis=-1)
+    X_val   = tf.expand_dims(X_val  , axis=-1)
+    X_test  = tf.expand_dims(X_test , axis=-1)
+
+    return X_train, X_test, X_val, Y_train, Y_test, Y_val, char_encoder, encoder_size
+
+
+def create_model(input_shape, encoder_size):
+    """
+    Helper function to instantiate a version of our model with correct inputs
+
+    Inputs:
+    input_shape - shape of the input CAPTCHAs
+    encoder_size - Number of unique characters that can be predicted
+
+    Output:
+    model - returns our model to be used
+    """
+    model = tf.keras.models.Sequential([
+            #CNN layers of our CRNN
             tf.keras.layers.Conv2D(32, (3,3), padding="same", input_shape=input_shape),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.LeakyReLU(),
@@ -32,27 +74,70 @@ class Model(tf.keras.Model):
             tf.keras.layers.Conv2D(128, (3,3), padding="same", activation="leaky_relu"),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+            tf.keras.layers.MaxPool2D(pool_size=(2,3)),
 
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(100, return_sequences=True)),
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(50 , return_sequences=True))
+            tf.keras.layers.Reshape((input_shape[-2] // 12, input_shape[-3] // 8 * 128)),
 
-            # tf.keras.layers.Flatten(),
-            # tf.keras.layers.Dense(256, activation='leaky_relu'),
-            # tf.keras.layers.Dense(self.num_classes, activation="softmax")
+            #RNN segment of our CRNN
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(input_shape[-3] // 8 * 128, return_sequences=True)),
+            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(input_shape[-3] // 8 * 128, return_sequences=True)),
+
+            # +1 IS NECESSARY for CTC loss.
+            tf.keras.layers.Dense(encoder_size+1, activation="softmax")
         ])
+    return model
 
 
-    def call(self, inputs):
-        return self.model(inputs)
+# CTC defined loss function for calculated loss of a CRNN.
+def ctc(y_true, y_pred):
+    """
+    A custom loss function built off of Keras' ctc_batch_cost which calculates loss for our CRNN
+
+    Inputs:
+    y_true - True labels
+    y_pred - Predicted labels
+
+    Output:
+    loss - Loss as calculated by ctc_batch_cost
+    """
+    # Cast true labels to ints
+    y_true = tf.cast(y_true, tf.int32)
+
+    # Calculate label_length and input_length sizes for ctc_batch_cost by appending batch size
+    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+    input_len = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+    label_len = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+    input_length = input_len * tf.ones(shape=(batch_len, 1), dtype="int64")
+    label_length = label_len * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, label_length=label_length, input_length=input_length)
+
+    return loss
 
 
 def get_accuracy(model, X_test, Y_test):
+    """
+    Calculates model accuracy
+
+    Inputs:
+    model - Model you are testing accuracy on
+    X_test - CAPTCHAs to test accuracy on
+    Y_test - True text for each input CAPTCHA
+
+    Output:
+    accuracy - Percentage of the time the model correctly reads the entire CAPTCHA
+    """
+    # Use model to predict text
     probs = model.predict(X_test, verbose=0)
     output = np.argmax(probs, axis=2)
 
+    # The output is from an LSTM which looks something like [1,1,31,25,7,7]
+    # This takes it down to the four unique characters predicted in a row
     prediction = np.asarray([np.asarray(tf.unique_with_counts(x)[0]) for x in output])
     Y_test = np.asarray(Y_test)
+
+    # Calculate accuracy by comparing predictions with truth.
     count = 0
     total = Y_test.shape[0]
     for x, y in zip(prediction, Y_test):
@@ -61,82 +146,47 @@ def get_accuracy(model, X_test, Y_test):
 
     return accuracy
 
-def create_model(input_shape, ohe_size):
-    model = tf.keras.models.Sequential([
-            tf.keras.layers.Conv2D(32, (3,3), padding="same", input_shape=input_shape),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
 
-            tf.keras.layers.Conv2D(64, (3,3), padding="same", activation="leaky_relu"),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,2)),
+def print_results(model, char_encoder, X_test, Y_test):
+    """
+    Prints an example CAPTCHA prediction vs. actual and prints accuracy
 
-            tf.keras.layers.Conv2D(128, (3,3), padding="same", activation="leaky_relu"),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(),
-            tf.keras.layers.MaxPool2D(pool_size=(2,3)), # output size: 3,3,128
+    Inputs:
+    model - Model to get accuracy and example with
+    char_encoder - one hot encoder used to labels of dataset
+    X_test - CAPTCHA images to test with
+    Y_test - True CAPTCHA labels to compare to
+    """
+    output = model.predict(X_test[1:2], verbose=0)
+    output = np.transpose(output, axes=[1, 0, 2]).squeeze()
+    output= np.argmax(output, axis=1)
+    prediction_captcha = np.asarray([np.asarray(tf.unique_with_counts(x)[0]) for x in output])
 
-            tf.keras.layers.Reshape((input_shape[-2] // 12, input_shape[-3] // 8 * 128)),
+    # Decode label's one hot encoding
+    real_captcha = char_encoder.inverse_transform(Y_test[1])
 
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(input_shape[-3] // 8 * 128, return_sequences=True)),
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(input_shape[-3] // 8 * 128, return_sequences=True)),
+    # Get accuracy
+    accuracy = get_accuracy(model, X_test, Y_test)
 
-            tf.keras.layers.Dense(ohe_size+1, activation="softmax")
-        ])
-    return model
+    #Print example CAPTCHA and accuracy
+    print(f"Example CAPTCHA:     {real_captcha}")
+    print(f"Example Model Guess: {prediction_captcha}")
+    print(f"Accuracy: {accuracy}")
+
 
 def main():
-    #Import and reshape
-    X_train, X_test, X_val, Y_train, Y_test, Y_val = get_data_ocr(.3, "./processed_whole_data/")
+    X_train, X_test, X_val, Y_train, Y_test, Y_val, char_encoder, encoder_size = \
+        retrieve_data(.3, "./processed_whole_data/")
 
-    char_encoder = preprocessing.LabelEncoder().fit(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1))))
-    ohe_size = np.max(char_encoder.transform(np.concatenate((Y_train.reshape(-1), Y_test.reshape(-1), Y_val.reshape(-1)))))
-    orig_shapes = [Y_train.shape[0], Y_test.shape[0], Y_val.shape[0]]
-    Y_train = char_encoder.transform(Y_train.reshape(-1)).reshape((orig_shapes[0], 4))
-    Y_test = char_encoder.transform(Y_test.reshape(-1)).reshape((orig_shapes[1], 4))
-    Y_val = char_encoder.transform(Y_val.reshape(-1)).reshape((orig_shapes[2], 4))
-
-    #Convert input to properly shaped and typed tensors
-    X_train  = tf.convert_to_tensor(np.asarray(X_train).astype(np.float32))
-    Y_train  = tf.convert_to_tensor(np.asarray(Y_train))
-    X_val  = tf.convert_to_tensor(np.asarray(X_val).astype(np.float32))
-    Y_val  = tf.convert_to_tensor(np.asarray(Y_val))
-
-    #Don't reshape test, because we need to preserve CAPTCHAs but convert to tensor
-    X_test  = tf.convert_to_tensor(np.asarray(X_test).astype(np.float32))
-    Y_test  = tf.convert_to_tensor(np.asarray(Y_test))
-
-    #Expand Dims
-    X_train = tf.expand_dims(X_train, axis=-1)
-    X_val   = tf.expand_dims(X_val  , axis=-1)
-    X_test  = tf.expand_dims(X_test , axis=-1)
-
+    # Input shape is the shape of X_train without batch_size attached
     input_shape=(X_train.shape[-3], X_train.shape[-2], X_train.shape[-1])
-    print(input_shape)
 
-
-    def ctc(y_true, y_pred):
-        y_true = tf.cast(y_true, tf.int32)
-        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
-
-        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-
-        #input_length = tf.math.reduce_sum(y_pred, axis=-1, keepdims=False)
-        #label_length = tf.math.count_nonzero(y_true, axis=-1, keepdims=False, dtype=tf.int32)
-
-        return tf.keras.backend.ctc_batch_cost(y_true, y_pred, label_length=label_length, input_length=input_length)
-    # Compile and fit model
-    model = create_model(input_shape, (ohe_size+1))
+    # Fit model
+    # NOTE: encoder_size incremented by 1 is necessary here AND inside the model for CTC_loss to work
+    # Having it twice is NOT an error.
+    model = create_model(input_shape, (encoder_size+1))
     model.compile(loss=ctc,
                     optimizer=tf.keras.optimizers.Adam(.0001))
-
-    model.summary()
-
     model.fit(
         X_train,
         Y_train,
@@ -148,23 +198,8 @@ def main():
     # Save model for future testing
     model.save('./models/ocr', save_format="h5")
 
-    loaded_model = tf.keras.models.load_model("./models/ocr", custom_objects={'ctc': ctc})
-    print(loaded_model.summary())
+    print_results(model, char_encoder, X_test, Y_test)
 
-    # Run random CAPTCHA test
-    output = model.predict(X_test[1:2], verbose=0)
-    print("Secret CAPTCHA:")
-    print(char_encoder.inverse_transform(Y_test[1]))
-    print("Model guess:")
-    output = np.transpose(output, axes=[1, 0, 2]).squeeze()
-    print(output)
-    print(np.argmax(output, axis=1))
-
-    prediction = tf.unique_with_counts(np.argmax(output, axis=1))
-    print(prediction)
-
-    print("Accuracy:")
-    print(get_accuracy(model, X_test, Y_test))
 
 
 if __name__ == '__main__':
