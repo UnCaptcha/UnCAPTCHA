52e56b5f333408ad8e62f2b44fa483eda6602fe0
Mahlon Page <mahlon_page@brown.edu>
2023-05-08 02:07:16 -0400

    final code

--- code/main_ocr.py
+++ code/main_ocr.py
@@ -55,18 +55,23 @@ def ctc(y_true, y_pred):
     Output:
     loss - Loss as calculated by ctc_batch_cost
     """
+
     # Cast true labels to ints
     y_true = tf.cast(y_true, tf.int32)
 
     # Calculate label_length and input_length sizes for ctc_batch_cost by appending batch size
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    input_len = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    label_len = tf.cast(tf.shape(y_true)[1], dtype="int64")
+    y_pred_shape = tf.cast(tf.shape(y_pred)[1], dtype = tf.int32)
+    y_true_shape = tf.cast(tf.shape(y_true)[1], dtype = tf.int32)
+    batch_size   = tf.cast(tf.shape(y_true)[0], dtype = tf.int32)
 
-    input_length = input_len * tf.ones(shape=(batch_len, 1), dtype="int64")
-    label_length = label_len * tf.ones(shape=(batch_len, 1), dtype="int64")
+    # Make an extender of all ones with proper shape
+    y_pred_extender = tf.ones(shape = (batch_size, 1), dtype = tf.int32)
+    y_true_extender = tf.ones(shape = (batch_size, 1), dtype = tf.int32)
 
-    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, label_length=label_length, input_length=input_length)
+    input_length = y_pred_shape * y_pred_extender
+    label_length = y_true_shape * y_true_extender
+
+    loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, label_length = label_length, input_length = input_length)
 
     return loss
 
@@ -90,6 +95,7 @@ def get_accuracy(model, X_test, Y_test):
     # The output is from an LSTM which looks something like [1,1,31,25,7,7]
     # This takes it down to the four unique characters predicted in a row
     prediction = np.asarray([np.asarray([i[0] for i in groupby(x)]) for x in output])
+    prediction = np.asarray([x[x != 32] for x in prediction]) #Filters out '_'
     Y_test = np.asarray(Y_test)
 
     # Calculate accuracy by comparing predictions with truth.
@@ -115,6 +121,7 @@ def print_results(model, X_test, Y_test):
     output = np.transpose(output, axes=[1, 0, 2]).squeeze()
     output= np.argmax(output, axis=1)
     prediction_captcha = np.asarray([i[0] for i in groupby(output)])
+    prediction_captcha = prediction_captcha[prediction_captcha != 32]
 
     # Decode label's one hot encoding
     alphabet_key = dict(zip(range(0, 33), list('23456789ABCDEFGHJKLMNPQRSTUVWXYZ_')))
@@ -134,25 +141,26 @@ def main():
     encoder_size = 31
     X_train, Y_train, X_test, Y_test, X_val, Y_val = \
         retrieve_data_ocr("./../data/ocr_data_split/")
+
     # Input shape is the shape of X_train without batch_size attached
     input_shape = (X_train.shape[-3], X_train.shape[-2], X_train.shape[-1])
 
     # Fit model
     # NOTE: encoder_size incremented by 1 is necessary here AND inside the model for CTC_loss to work
     # Having it twice is NOT an error.
-    # model = create_model(input_shape, (encoder_size+1))
-    # model.compile(loss=ctc,
-    #                 optimizer=tf.keras.optimizers.Adam(.0001))
-    # model.fit(
-    #     X_train,
-    #     Y_train,
-    #     epochs=80,
-    #     batch_size=256,
-    #     validation_data=(X_val, Y_val)
-    # )
+    model = create_model(input_shape, (encoder_size+1))
+    model.compile(loss=ctc,
+                    optimizer=tf.keras.optimizers.Adam(.0001))
+    model.fit(
+        X_train,
+        Y_train,
+        epochs=80,
+        batch_size=256,
+        validation_data=(X_val, Y_val)
+    )
 
-    # # Save model for future testing
-    # model.save('./../models/ocr', save_format="h5")
+    # Save model for future testing
+    model.save('./../models/ocr', save_format="h5")
 
     model = tf.keras.models.load_model("./../models/ocr", custom_objects={"ctc": ctc})
     print_results(model, X_test, Y_test)
